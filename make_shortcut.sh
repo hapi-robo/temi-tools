dd26672ce0f93a50eed596c52f05fe205c25cdde
Mana Yama <angelica00705@gmail.com>
2019-12-19 22:37:11 +0900

    Adds gitignore; Checks arguments; Fixes sed; Uses curl as an alternative to wget

--- make_shortcut.sh
+++ make_shortcut.sh
@@ -10,68 +10,96 @@
 #
 #
 
+# constants
 GIT_REPO_NAME="temi-app-shortcut"
 VERSION="1.0.0"
-APK_FILENAME=$1
 
-# expect 2 arguments
+# debug
+SHORTCUT_NAME="test"
 
+usage ()
+{
+  echo ''
+  echo 'usage: make_shortcut.sh apk_filename shortcut_name'
+  echo ''
+  echo 'Creates a shortcut for APK file'
+  echo ''
+  echo 'positional arguments:'
+  echo ''
+  echo '  apk_filename          APK filename (.apk)'
+  echo '  shortcut_name         Shortcut name; use double-quotes to encapsulate'
+  echo '                        a name with whitespace'
+  echo ''
+  exit
+}
+
+# # check for APK file
+# if [ -z "$1" ]; then
+#   echo "Missing APK file"
+#   usage
+# else
+#   APK_FILENAME=$1
+# fi
+
+# # check for shortcut name
+# if [ -z "$2" ]; then
+#   echo "Missing shortcut name"
+#   usage
+# else
+#   SHORTCUT_NAME=$2
+# fi
+
+# # check dependencies
+# echo "Check for dependencies..."
+# if ! command -v aapt 2>/dev/null; then
+#   echo "Missing dependencies"
+#   exit
+# fi
 
 # lower case all letters
 # substitute whitespace ' ' with underscore '_'
-SHORTCUT_NAME="$(echo $2 | tr '[:upper:]' '[:lower:]' | tr ' ' '_')"
-echo "${SHORTCUT_NAME}"
-
-# TEMI_APP_SHORTCUT="temi-app-shortcut"
-
-# check dependencies
-echo "Check for dependencies..."
-if ! command -v aapt 2>/dev/null; then
-	echo "Missing dependencies"
-fi
+# SHORTCUT_NAME="$(echo $2 | tr '[:upper:]' '[:lower:]' | tr ' ' '_')"
+# echo "${SHORTCUT_NAME}"
 
 # get package name
 # aapt dump badging $1 | grep package:\ name
-PACKAGE_NAME="$(aapt dump badging "${APK_FILENAME}" | awk -F "[='|' ]" '/package: / {print $4}')"
-echo "${PACKAGE_NAME}"
+# PACKAGE_NAME="$(aapt dump badging "${APK_FILENAME}" | awk -F "[='|' ]" '/package: / {print $4}')"
+# echo "${PACKAGE_NAME}"
 
 # get sample
 echo "Downloading..."
-wget -nv "https://github.com/ray-hrst/${GIT_REPO_NAME}/archive/v${VERSION}.tar.gz"
+curl -LJO "https://github.com/ray-hrst/${GIT_REPO_NAME}/archive/v${VERSION}.tar.gz"
+# wget --no-check-certificate --content-disposition https://github.com/ray-hrst/temi-app-shortcut/archive/v1.0.0.tar.gz
 
-# decompress
-echo "Decompressing..."
-tar -xzf "v${VERSION}.tar.gz" 
-rm "v${VERSION}.tar.gz"
+# # decompress
+# echo "Decompressing..."
+tar -xzf "${GIT_REPO_NAME}-${VERSION}.tar.gz"
+rm "${GIT_REPO_NAME}-${VERSION}.tar.gz"
 
 # enter shortcut source code root directory
 cd "${GIT_REPO_NAME}-${VERSION}"
-echo "${PWD}"
 
 # rename package
-# mv app/src/main/java/com/hapirobo/shortcut "app/src/main/java/com/hapirobo/shortcut_${SHORTCUT_NAME}" 
-# mv app/src/androidTest/java/com/hapirobo/shortcut "app/src/androidTest/java/com/hapirobo/shortcut_${SHORTCUT_NAME}" 
-# mv app/src/test/java/com/hapirobo/shortcut "app/src/test/java/com/hapirobo/shortcut_${SHORTCUT_NAME}" 
+mv -v app/src/main/java/com/hapirobo/shortcut "app/src/main/java/com/hapirobo/shortcut_${SHORTCUT_NAME}"
+mv -v app/src/androidTest/java/com/hapirobo/shortcut "app/src/androidTest/java/com/hapirobo/shortcut_${SHORTCUT_NAME}"
+mv -v app/src/test/java/com/hapirobo/shortcut "app/src/test/java/com/hapirobo/shortcut_${SHORTCUT_NAME}"
 
-# modify files
-app/build.gradle
-# echo "package=\"com.hapirobo.shortcut\"" | sed 's/package=\"\(.*\)\"/package=\"com.oung.ray\"/'
-cat app/build.gradle | sed 's/shortcut/shortcut_${SHORTCUT_NAME}/'
-app/src/main/AndroidManifest.xml
+# modify files inline
+# echo "package=\"com.hapirobo.shortcut\"" | sed 's/shortcut/shortcut_test/'
+sed -i .bak "s/shortcut/shortcut_${SHORTCUT_NAME}/" app/build.gradle
+sed -i .bak "s/shortcut/shortcut_${SHORTCUT_NAME}/" app/src/main/AndroidManifest.xml
 
-
-# # build gradle
+# # build shortcut APK
 # ./gradlew build
+
+# move file to root directory
 # cp -v app/build/outputs/apk/debug/app-debug.apk "../${SHORTCUT_NAME}.apk"
-
-
-
 
 # clean up
 # echo "Cleaning up..."
 # rm -fr "${VERSION}"
 
-# echo "Done"
+echo "Done"
 
 
 # ========================================
